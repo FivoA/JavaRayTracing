d7135232ed1203786d9428c36f646924a4faa78a
fivoA <fivo.ahmed23@gmail.com>
2024-08-06 20:39:14 +0200

    Integrated Multithreaded rendering

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,9 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import static Math_Util.Sphere.intersectSphere;
 import static Math_Util.color.*;
@@ -46,17 +49,41 @@ public class Main {
         BufferedWriter writer = new BufferedWriter(new FileWriter("image.ppm"));
 
         writer.write("P3"+ "\n" + image_width + " " + image_height + "\n255\n");
-        for (int j = 0; j < image_height; j++) {
-            for (int i = 0; i < image_width; i++) {
-                vec3 pixelCenter = add(pixel0_location,multiply(deltaRight,i));
-                pixelCenter =  add(pixelCenter, multiply(deltaDown,j));
-                vec3 rayDir = subtract(pixelCenter,camera_center);
 
-                Ray ray = new Ray(camera_center,rayDir);
-                color col = rayColor(ray);
-                writeColor(writer,col);
-            }
-        }
+        //long startTime = System.nanoTime(); // Start time
+        renderImage(image_width,image_height,pixel0_location,deltaRight,deltaDown,camera_center,writer);
+        //long endTime = System.nanoTime(); // End time
+
         writer.close();
+        //long duration = (endTime - startTime) / 1_000_000; // MS conversion
+        //System.out.println("Rendering took: " + duration + " ms");
+
+    }
+
+    private static final int NUM_CORES = Runtime.getRuntime().availableProcessors();
+    public static void renderImage(int imageWidth, int imageHeight, vec3 pixel0Location, vec3 deltaRight, vec3 deltaDown, point3 cameraCenter, BufferedWriter writer) {
+        ExecutorService executor = Executors.newFixedThreadPool(NUM_CORES);
+        for (int j = 0; j < imageHeight; j++) {
+            final int row = j;
+            executor.submit(() -> {
+                try {
+                    renderRow(row, imageWidth, pixel0Location, deltaRight, deltaDown, cameraCenter, writer);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        }
+        executor.shutdown();
+    }
+    private static void renderRow(int row, int imageWidth, vec3 pixel0Location, vec3 deltaRight, vec3 deltaDown, point3 cameraCenter, BufferedWriter writer) throws IOException {
+        for (int i = 0; i < imageWidth; i++) {
+            vec3 pixelCenter = add(pixel0Location, multiply(deltaRight, i));
+            pixelCenter = add(pixelCenter, multiply(deltaDown, row));
+            vec3 rayDir = subtract(pixelCenter, cameraCenter);
+
+            Ray ray = new Ray(cameraCenter, rayDir);
+            color col = rayColor(ray);
+            writeColor(writer, col);
+        }
     }
 }
